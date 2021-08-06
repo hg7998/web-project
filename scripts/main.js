ad09ae947be23652533534b6de6b73c902a259b4
Hunter Guthrie <hg7998@yahoo.com>
2021-08-06 11:08:04 -0500

    saving progress

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,22 @@
-window.onload = function () {
-    document.getElementById('dogsOrCatsSubmit').addEventListener('click', function (event) {
-        if (document.getElementById('Cats').checked) {
-            document.getElementById('dogsOrCats').innerText = 'Wrong choice.'
-            //PLAY STEVE CARREL "NO" GIF//
-            
-            /*let i = 5;
-            var timer = setInterval(function () {
-                if (i <= 0) {
-                    clearInterval(timer);
-                }
-                document.getElementById('dogsOrCats').innerText = 'Wrong choice. You will be redirected shortly... ' + i;
-                i--;
-            }, 1000);
+document.getElementById('dogsOrCatsSubmit').addEventListener('click', function (event) {
+    if (document.getElementById('Cats').checked) {
+        document.getElementById('dogsOrCats').innerText = 'Wrong choice.'
+        //PLAY STEVE CARREL "NO" GIF//
 
-            setTimeout(function () {
-                window.close();
-            }, 6000); */
-        }
-        else if (document.getElementById('Dogs').checked) {
-            document.getElementById('dogsOrCats').innerText = 'You made the right choice!'
-        }
-    });
+        /*let i = 5;
+        var timer = setInterval(function () {
+            if (i <= 0) {
+                clearInterval(timer);
+            }
+            document.getElementById('dogsOrCats').innerText = 'Wrong choice. You will be redirected shortly... ' + i;
+            i--;
+        }, 1000);
 
-};
+        setTimeout(function () {
+            window.close();
+        }, 6000); */
+    }
+    else if (document.getElementById('Dogs').checked) {
+        document.getElementById('dogsOrCats').innerText = 'You made the right choice!'
+    }
+});
