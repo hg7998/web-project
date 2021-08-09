e03a6bda9ffa0a2343e817bd13849b269ccef502
Hunter Guthrie <hg7998@yahoo.com>
2021-08-09 14:23:51 -0500

    trying to fix branch problem

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,20 @@ document.getElementById('dogsOrCatsSubmit').addEventListener('click', function (
     event.preventDefault();
     if (document.getElementById('Cats').checked) {
         document.getElementById('dogsOrCatsResponse').innerText = 'Wrong choice.'
-        //PLAY STEVE CARREL "NO" GIF//
-
+        
         /*let i = 5;
+        document.getElementById('dogsOrCatsResponse').innerText = 'Wrong choice. You will be redirected shortly... ';
+        
         var timer = setInterval(function () {
             if (i <= 0) {
                 clearInterval(timer);
             }
-            document.getElementById('dogsOrCats').innerText = 'Wrong choice. You will be redirected shortly... ' + i;
+            document.getElementById('dogsOrCatsResponse').innerText = 'Wrong choice. You will be redirected shortly... ' + i;
             i--;
         }, 1000);
 
         setTimeout(function () {
-            window.close();
+            /*window.close();
         }, 6000); */
     }
     else if (document.getElementById('Dogs').checked) {
