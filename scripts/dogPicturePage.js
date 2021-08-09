1d25e56adde04b2cff134fab11bc71b1afe916c7
Hunter Guthrie <hg7998@yahoo.com>
2021-08-09 18:43:38 -0500

    added scripts for main page and dogPicturePage

---
+++ scripts/dogPicturePage.js
@@ -0,0 +1,112 @@
+let dogPictureUrl = "https://random.dog/woof.json";
+
+document.getElementById('dogFactsAndPicturesButton').addEventListener('click', function (event) {
+
+    fetch(dogPictureUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error("Could not fetch image");
+            }
+            return response.json();
+        })
+        .then(function (data) {
+
+            if (document.getElementById("dogPictureOne").hasChildNodes) {
+                var parentNode = document.getElementById("dogPictureOne");
+                var childNode = parentNode.firstChild;
+                parentNode.removeChild(childNode);
+            }
+
+            let imageFormat = data.url.substring(data.url.length - 4);
+            if (imageFormat === '.mp4' || imageFormat === 'webm') {
+                const video = document.createElement('video');
+                video.controls = true;
+                video.autoplay = true;
+                video.src = data.url;
+                video.setAttribute('id', 'video');
+                document.getElementById('dogPictureOne').appendChild(video);
+            }
+            else {
+                const img = document.createElement('img');
+                img.src = data.url;
+                document.getElementById('dogPictureOne').appendChild(img);
+            }
+
+            fetch(dogPictureUrl)
+                .then(function (response) {
+                    if (!response.ok) {
+                        throw new Error("Could not fetch image");
+                    }
+                    return response.json();
+                })
+                .then(function (data) {
+
+                    if (document.getElementById("dogPictureTwo").hasChildNodes) {
+                        var parentNode = document.getElementById("dogPictureTwo");
+                        var childNode = parentNode.firstChild;
+                        parentNode.removeChild(childNode);
+                    }
+
+                    let imageFormat = data.url.substring(data.url.length - 4);
+                    if (imageFormat === '.mp4' || imageFormat === 'webm') {
+                        const video = document.createElement('video');
+                        video.controls = true;
+                        video.autoplay = true;
+                        video.src = data.url;
+                        video.setAttribute('id', 'video');
+                        document.getElementById('dogPictureTwo').appendChild(video);
+                    }
+                    else {
+                        const img = document.createElement('img');
+                        img.src = data.url;
+                        document.getElementById('dogPictureTwo').appendChild(img);
+                    }
+
+                    fetch(dogPictureUrl)
+                        .then(function (response) {
+                            if (!response.ok) {
+                                throw new Error("Could not fetch image");
+                            }
+                            return response.json();
+                        })
+                        .then(function (data) {
+
+                            if (document.getElementById("dogPictureThree").hasChildNodes) {
+                                var parentNode = document.getElementById("dogPictureThree");
+                                var childNode = parentNode.firstChild;
+                                parentNode.removeChild(childNode);
+                            }
+
+                            let imageFormat = data.url.substring(data.url.length - 4);
+                            if (imageFormat === '.mp4' || imageFormat === 'webm') {
+                                const video = document.createElement('video');
+                                video.controls = true;
+                                video.autoplay = true;
+                                video.src = data.url;
+                                video.setAttribute('id', 'video');
+                                document.getElementById('dogPictureThree').appendChild(video);
+                            }
+                            else {
+                                const img = document.createElement('img');
+                                img.src = data.url;
+                                document.getElementById('dogPictureThree').appendChild(img);
+                            }
+                        })
+                        .catch(function () {
+                            const error = document.createElement('p');
+                            error.innerText = "Sorry, there was an error loading the image."
+                            document.getElementById(dogPictureThree).appendChild(error);
+                        })
+                        .catch(function () {
+                            const error = document.createElement('p');
+                            error.innerText = "Sorry, there was an error loading the image."
+                            document.getElementById(dogPictureTwo).appendChild(error);
+                        })
+                })
+                .catch(function () {
+                    const error = document.createElement('p');
+                    error.innerText = "Sorry, there was an error loading the image."
+                    document.getElementById(dogPictureOne).appendChild(error);
+                })
+        })
+});
--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ document.getElementById('dogsOrCatsSubmit').addEventListener('click', function (
     event.preventDefault();
     if (document.getElementById('Cats').checked) {
         document.getElementById('dogsOrCatsResponse').innerText = 'Wrong choice.'
-        
-        /*let i = 5;
+
+        let i = 5;
         document.getElementById('dogsOrCatsResponse').innerText = 'Wrong choice. You will be redirected shortly... ';
-        
+
         var timer = setInterval(function () {
             if (i <= 0) {
                 clearInterval(timer);
@@ -15,10 +15,63 @@ document.getElementById('dogsOrCatsSubmit').addEventListener('click', function (
         }, 1000);
 
         setTimeout(function () {
-            /*window.close();
-        }, 6000); */
+            /*window.close();*/
+        }, 6000);
     }
     else if (document.getElementById('Dogs').checked) {
         document.getElementById('dogsOrCatsResponse').innerText = 'You made the right choice!'
     }
 });
+
+let pictureUrl = "https://random.dog/woof.json";
+let factUrl = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all";
+
+window.onload = function () {
+    fetch(pictureUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error("Could not fetch image");
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            let imageFormat = data.url.substring(data.url.length - 4);
+            if (imageFormat === '.mp4' || imageFormat === 'webm') {
+                const video = document.createElement('video');
+                video.controls = true;
+                video.autoplay = true;
+                video.src = data.url;
+                video.setAttribute('id','video');
+                document.getElementById('imageContainer').appendChild(video);
+            }
+            else {
+                const img = document.createElement('img');
+                img.src = data.url;
+                document.getElementById('imageContainer').appendChild(img);
+            }
+
+            fetch(factUrl)
+                .then(function (response) {
+                    if (!response.ok) {
+                        throw new Error("Could not fetch fact");
+                    }
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    const fact = document.createElement('p');
+                    fact.innerText = obj.fact;
+                    document.getElementById('dogFactContainer').appendChild(fact);
+                })
+                .catch(function () {
+                    const error = document.createElement('p');
+                    error.innerText = "Sorry, there was an error loading the random fact."
+                    document.getElementById('dogFactContainer').appendChild(error);
+                })
+                .catch(function () {
+                    const error = document.createElement('p');
+                    error.innerText = "Sorry, there was an error loading the image."
+                    document.getElementById('imageContainer').appendChild(error);
+                })
+        });
+};
